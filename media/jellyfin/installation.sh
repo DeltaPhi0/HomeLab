7058ba51af74c393b2fab28de7409a71e3726ba3
Ken J. S. <kenetsokoli601@gmail.com>
2025-05-21 08:10:41 +0200

    Update installation.sh

--- media/jellyfin/installation.sh
+++ media/jellyfin/installation.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
+curl https://repo.jellyfin.org/install-debuntu.sh | bash
 sudo mkdir -p /media/jellyfin/{movies,shows}
 sudo chown -R $USER:$USER /media/jellyfin
 sudo chmod -R 755 /media/jellyfin
