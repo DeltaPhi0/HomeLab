6fe7bb046d2ad4996ea22cf6cce1f2faf44e5a85
Ken J. S. <kenetsokoli601@gmail.com>
2025-05-21 09:53:09 +0200

    Update installation.sh

--- media/jellyfin/installation.sh
+++ media/jellyfin/installation.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+sudo apt update
 curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
-mkdir -p /media/jellyfin/{movies,shows}
-chown -R $USER:$USER /media/jellyfin
-chmod -R 755 /media/jellyfin
+sudo mkdir -p /media/jellyfin/{movies,shows}
+echo "What is your machine's username?"
+read USR
+sudo chown -R $USR:$USR /media/jellyfin
+sudo chmod -R 755 /media/jellyfin
+sudo apt upgrade -y
