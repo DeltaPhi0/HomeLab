4066f9a1c9b95aa21d1ac8901b6c0fce3223956b
Ken J. S. <kenetsokoli601@gmail.com>
2025-05-21 10:13:01 +0200

    Update jellyfin-setup.sh

--- media/jellyfin/jellyfin-setup.sh
+++ media/jellyfin/jellyfin-setup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+echo "What is your machine's username?"
+read USR
+read -p "Do you want to read the script? [y/n]: " answer
+
+if [[ "$answer" =~ ^[Yy]$ ]]; then
+    curl -s https://raw.githubusercontent.com/DeltaPhi0/homelab/refs/heads/main/media/jellyfin/jellyfin-setup.sh
+elif [[ "$answer" =~ ^[Nn]$ ]]; then
+    echo "Then let's proceed."
+else
+    echo "Invalid input. Please enter y or n."
+fi
+
 sudo apt update
 curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
 sudo mkdir -p /media/jellyfin/{movies,shows}
-echo "What is your machine's username?"
-read USR
 sudo chown -R $USR:$USR /media/jellyfin
 sudo chmod -R 755 /media/jellyfin
 sudo apt upgrade -y
